724b8798ea18b5aad11ea8c7fb76d0a2f1a0136c
npedotnet <npedotnet@gmail.com>
2024-02-26 06:44:45 +0900

    空白をTABで置き換え

--- unity/PopupWindow.cs
+++ unity/PopupWindow.cs
@@ -10,37 +10,37 @@ using UnityEngine;
 
 public class PopupWindow : MonoBehaviour
 {
-    [DllImport("user32.dll")]
-    static extern IntPtr GetActiveWindow();
+	[DllImport("user32.dll")]
+	static extern IntPtr GetActiveWindow();
 
-    [DllImport("user32.dll")]
-    static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+	[DllImport("user32.dll")]
+	static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
 
-    [DllImport("user32.dll")]
-    static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+	[DllImport("user32.dll")]
+	static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
 
-    const int GWL_STYLE = -16;
-    const uint WS_VISIBLE = 0x10000000;
-    const uint WS_POPUP = 0x80000000;
-    const int HWND_TOPMOST = -1;
-    const uint SWP_NOMOVE = 0x0002;
+	const int GWL_STYLE = -16;
+	const uint WS_VISIBLE = 0x10000000;
+	const uint WS_POPUP = 0x80000000;
+	const int HWND_TOPMOST = -1;
+	const uint SWP_NOMOVE = 0x0002;
 
-    void Start()
-    {
+	void Start()
+	{
 #if !UNITY_EDITOR
-        int width = Screen.width;
-        int height = Screen.height;
-        var hWnd = GetActiveWindow();
-        SetWindowLong(hWnd, GWL_STYLE, WS_VISIBLE | WS_POPUP);
-        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, width, height, SWP_NOMOVE);
+		int width = Screen.width;
+		int height = Screen.height;
+		var hWnd = GetActiveWindow();
+		SetWindowLong(hWnd, GWL_STYLE, WS_VISIBLE | WS_POPUP);
+		SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, width, height, SWP_NOMOVE);
 #endif
-    }
+	}
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-    }
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
 }
--- unity/TransparentWindow.cs
+++ unity/TransparentWindow.cs
@@ -10,46 +10,46 @@ using UnityEngine;
 
 public class TransparentWindow : MonoBehaviour
 {
-    [DllImport("user32.dll")]
-    static extern IntPtr GetActiveWindow();
+	[DllImport("user32.dll")]
+	static extern IntPtr GetActiveWindow();
 
-    [DllImport("user32.dll")]
-    static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+	[DllImport("user32.dll")]
+	static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
 
-    [DllImport("user32.dll")]
-    static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+	[DllImport("user32.dll")]
+	static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
 
-    [DllImport("user32.dll")]
-    static extern int SetLayeredWindowAttributes(IntPtr hWnd, int crKey, byte bAlpha, uint dwFlags);
+	[DllImport("user32.dll")]
+	static extern int SetLayeredWindowAttributes(IntPtr hWnd, int crKey, byte bAlpha, uint dwFlags);
 
-    const int GWL_STYLE = -16;
-    const uint WS_VISIBLE = 0x10000000;
-    const uint WS_POPUP = 0x80000000;
-    const int HWND_TOPMOST = -1;
-    const uint SWP_NOMOVE = 0x0002;
-    const int GWL_EXSTYLE = -20;
-    const uint WS_EX_LAYERED = 0x00080000;
-    const int LWA_COLORKEY = 1;
+	const int GWL_STYLE = -16;
+	const uint WS_VISIBLE = 0x10000000;
+	const uint WS_POPUP = 0x80000000;
+	const int HWND_TOPMOST = -1;
+	const uint SWP_NOMOVE = 0x0002;
+	const int GWL_EXSTYLE = -20;
+	const uint WS_EX_LAYERED = 0x00080000;
+	const int LWA_COLORKEY = 1;
 
-    void Start()
-    {
+	void Start()
+	{
 #if !UNITY_EDITOR
-        int width = Screen.width;
-        int height = Screen.height;
-        var hWnd = GetActiveWindow();
-        SetWindowLong(hWnd, GWL_STYLE, WS_VISIBLE | WS_POPUP);
-        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, width, height, SWP_NOMOVE);
+		int width = Screen.width;
+		int height = Screen.height;
+		var hWnd = GetActiveWindow();
+		SetWindowLong(hWnd, GWL_STYLE, WS_VISIBLE | WS_POPUP);
+		SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, width, height, SWP_NOMOVE);
 
-        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
-        SetLayeredWindowAttributes(hWnd, 0, 0, LWA_COLORKEY);
+		SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
+		SetLayeredWindowAttributes(hWnd, 0, 0, LWA_COLORKEY);
 #endif
-    }
+	}
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-    }
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
 }
