944bccd4f7ac72173a0a28f48f762e21591730d3
npedotnet <npedotnet@gmail.com>
2024-03-01 01:37:30 +0900

    SingletonMonoBehaviour追加

---
+++ unity/SingletonMonoBehaviour.cs
@@ -0,0 +1,12 @@
+﻿/* 
+ * Unity用のSingletonMonoBehaviourのサンプルプログラムです。
+ * 詳細は https://unixo-lab.com/unity/singleton.html をご覧下さい。
+ */
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+{
+    public static T Instance { get; private set; }
+    protected SingletonMonoBehaviour() { }
+    protected void Awake() => Instance = (T)this;
+}
